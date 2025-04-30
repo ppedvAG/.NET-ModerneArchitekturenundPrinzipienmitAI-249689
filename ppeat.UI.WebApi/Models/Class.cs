5b3828d1dbd58298e744c6e59a6674a3ea4606e0
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-04-30 14:52:51 +0200

    Automatisch generierte WebApi gegen DbContext mit httpFile zum testen

---
+++ ppeat.UI.WebApi/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿namespace ppeat.UI.WebApi.Models
+{
+    /**
+     * In Modelle erstellen wir die sog. Dtos (Data Transfer Objects) welche die Domaenen-Modelle projezieren
+     * bzw. umwandeln/formatieren/ergaenzen usw.
+     * Praktisch ist auch eine Mapper Extension Klasse welche die Dtos in die Domaenen-Modelle umwandelt und vice versa.
+     */
+    public class CustomerDto // RestaurantDto, CommentDto usw.
+    {
+        // TOTDO
+    }
+}
